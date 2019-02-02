8c409cc33c8ef79762dbb6424b6aa84111caf3e7
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-02 17:55:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.join
+    last_element = "and #{array.pop}"
+    array.push(last_element)
+    array.join(", ")
   end
 end
